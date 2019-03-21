079141129525ecafe9d1a3e02b5f1b37e0aa2ea1
Andrew Mason <andrewmcodes@protonmail.com>
2019-03-20 22:21:44 -0400

    add dynamic task form inputs

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,18 +1,18 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
-import { Controller } from "stimulus"
+import {
+  Controller
+} from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = ["links", "template"]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+  }
+
+  add_association(event) {
+    event.preventDefault()
+
+    let content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime())
+    this.linksTarget.insertAdjacentHTML('beforebegin', content)
   }
 }
